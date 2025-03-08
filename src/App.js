580de2711770c46b661d642351d7351362ae91b8
Qifzer <qifzer@protonmail.com>
2025-03-08 13:48:19 +0000

    added a diff highlight color

--- src/App.js
+++ src/App.js
@@ -459,7 +459,7 @@ const DarkYahtzee = () => {
             padding: 12px !important;
           }
           * {
-            -webkit-tap-highlight-color: transparent !important;
+            -webkit-tap-highlight-color: #ff4081 !important;
           }
           .game-title {
             font-size: 28px !important;
